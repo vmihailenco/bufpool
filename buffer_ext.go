a795f52aa0833e7c079ab2def36bf8645f00775a
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-04-04 16:48:48 +0300

    Change Buffer.grow to use pool

--- buffer_ext.go
+++ buffer_ext.go
@@ -1,5 +1,7 @@
 package bufpool
 
+import "bytes"
+
 // Reset resets the buffer to be empty,
 // but it retains the underlying storage for use by future writes.
 // Reset is the same as Truncate(0).
@@ -15,4 +17,43 @@ func (b *Buffer) Reset(buf []byte) {
 
 func (b *Buffer) reset(pos int) {
 	b.Reset(b.buf[:pos])
+}
+
+// grow grows the buffer to guarantee space for n more bytes.
+// It returns the index where bytes should be written.
+// If the buffer can't grow it will panic with ErrTooLarge.
+func (b *Buffer) grow(n int) int {
+	m := b.Len()
+	// If buffer is empty, reset to recover space.
+	if m == 0 && b.off != 0 {
+		b.Reset(nil)
+	}
+	// Try to grow by means of a reslice.
+	if i, ok := b.tryGrowByReslice(n); ok {
+		return i
+	}
+	if b.buf == nil && n <= smallBufferSize {
+		b.buf = Get(n).buf
+		return 0
+	}
+	c := cap(b.buf)
+	if n <= c/2-m {
+		// We can slide things down instead of allocating a new
+		// slice. We only need m+n <= c to slide, but
+		// we instead let capacity get twice as large so we
+		// don't spend all our time copying.
+		copy(b.buf, b.buf[b.off:])
+	} else if c > maxInt-c-n {
+		panic(bytes.ErrTooLarge)
+	} else {
+		// Not enough space anywhere, we need to allocate.
+		tmp := Get(2*c + n)
+		copy(tmp.buf, b.buf[b.off:])
+		b.buf, tmp.buf = tmp.buf, b.buf
+		Put(tmp)
+	}
+	// Restore b.off and len(b.buf).
+	b.off = 0
+	b.buf = b.buf[:m+n]
+	return m
 }
